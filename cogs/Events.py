c5bbb4f548c58f6798269b87c3e9a9f0880b88a4
CrAzyScreamx <34142944+CrAzyScreamx@users.noreply.github.com>
2020-05-31 18:27:19 +0300

    Errors

--- cogs/Events.py
+++ cogs/Events.py
@@ -13,6 +13,12 @@ class Events(commands.Cog):
         await self.client.change_presence(activity=discord.Game(name='%h'))
         print("Bot is ready!")
 
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.author.send(error)
+            await ctx.message.delete()
+
 
 
 def setup(client):
